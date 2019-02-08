b35346461cbbcbb1a86b379339b8bb3f3dbd73b4
cgspyn868 <zawad_abdullah@hotmail.com>
2019-02-08 20:32:02 +0600

    Maal mama maal

---
+++ Booking/src/sample/ResultScene.java
@@ -0,0 +1,116 @@
+package sample;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import org.postgresql.core.SqlCommandType;
+
+import java.net.URL;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Observable;
+import java.util.ResourceBundle;
+
+public class ResultScene implements Initializable {
+
+    public TableView<Property> hotelsList;
+    public Button goBack;
+    public Button proceed;
+    public TableColumn propertyCol;
+    public TableColumn priceCol;
+    public TableColumn ratingCol;
+
+    public String getQueryString() {
+        String SQL = "select P.property_id pid, P.property_name pname, P.rating prating\n" +
+                "from properties P join cities C on (P.city_id=C.city_id)\n" +
+                "where P.property_type = ? and upper(C.city_name) like ?";
+
+        return SQL;
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("initializng result scene");
+
+        propertyCol.setCellValueFactory(new PropertyValueFactory<>("pname"));
+        priceCol.setCellValueFactory(new PropertyValueFactory<>("pprice"));
+        ratingCol.setCellValueFactory(new PropertyValueFactory<>("prating"));
+
+        ResultSet rs;
+        try (PreparedStatement stmt = Main.con.prepareStatement(getQueryString())) {
+            stmt.setInt(1, 1);
+            stmt.setString(2, "%"+SearchScene.searchString.toUpperCase()+"%");
+            rs = stmt.executeQuery();
+            displayProperties(rs);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // display actor information
+    }
+
+    void displayProperties(ResultSet rs) throws Exception {
+        System.out.println("rs");
+
+        while (rs.next()) {
+            String pid = rs.getString("pid");
+            String pname = rs.getString("pname");
+            String prating = rs.getString("prating");
+            System.out.println(pid + " " + pname + " " + prating);
+            hotelsList.getItems().add(new Property(pid, pname, "100", prating));
+        }
+    }
+
+    public void goBackClicked(ActionEvent event) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("search.fxml"));
+        Main.stage.setScene(new Scene(root, 600, 500));
+    }
+
+    public void proceedClicked(ActionEvent event) throws Exception {
+        Property property = hotelsList.getSelectionModel().getSelectedItem();
+        if (property==null) System.out.println("No property selected yet!");
+        else System.out.println("Selected " + property.pid + " " + property.pname);
+    }
+
+    public class Property {
+        private String pid;
+        private String pname;
+        private String pprice;
+        private String prating;
+
+        public Property(String pid, String pname, String pprice, String prating) {
+            this.pid = pid;
+            this.pname = pname;
+            this.pprice = pprice;
+            this.prating = prating;
+        }
+
+        public String getPid() {
+            return pid;
+        }
+
+        public String getPname() {
+            return pname;
+        }
+
+        public String getPprice() {
+            return pprice;
+        }
+
+        public String getPrating() {
+            return prating;
+        }
+    }
+
+}
--- Booking/src/sample/SearchScene.java
+++ Booking/src/sample/SearchScene.java
@@ -1,15 +1,38 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
-public class SearchScene {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SearchScene implements Initializable {
+    public Label welcomeText;
     public TextField searchBar;
     public Button searchButton;
     public RadioButton radio1;
     public RadioButton radio2;
+    public static String searchString;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        welcomeText.setText("Welcome, " + Infos.username);
+    }
+
+    public void searched(ActionEvent event) throws Exception {
+        searchString = searchBar.getText();
+
+        Parent root = FXMLLoader.load(getClass().getResource("result.fxml"));
+        Main.stage.setScene(new Scene(root, 600, 500));
+    }
+
 }
